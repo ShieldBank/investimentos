e683c9bdd9ce3b5bb41e440d62acb90ff7a6448e
GabrielVieira21 <gabriel.vieira@1pay.com.br>
2025-11-13 17:50:34 -0300

    fix pdf v14

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ import {
 } from "recharts";
 import { Label } from "./components/ui/label";
 
-import SHIELDBANK from "../public/SHIELDBANK.png";
+// import SHIELDBANK from "../public/SHIELDBANK.png";
 import Seta from "../assets/avanco-rapido.png";
 
 // import { Button } from "./components/ui/button";
@@ -357,7 +357,7 @@ function App() {
           <div className="   w-full flex justify-around gap-2 max-md:flex max-md:flex-col    ">
             <img
               className="w-[38%]  img max-sm:w-full min-lg:-mt-40  max-md:-mt-30"
-              src={SHIELDBANK}
+              src="/SHIELDBANK.png"
             />
 
             <Card className="bloco-inteiro w-[40%] card    max-md:w-full max-md:mb-10  h-full bg-[#020922] border-0 rounded-2xl p-10 text-[#162456] gap-3">
